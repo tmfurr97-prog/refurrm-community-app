bb7954c272a0f321ce1b390eec25fca96b96b296
Reese <notjunk@refurrm.org>
2025-12-22 09:54:11 -0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          router: ["react-router-dom"],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1500, // optional, just relaxes the warning
-  },
-}));
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
